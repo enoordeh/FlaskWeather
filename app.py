333ee3d49f989de0220bafc9bb0c9ad5f5ca08ea
Emil Noordeh <emiln@stanford.edu>
2020-09-07 17:21:10 -0700

    Added error control

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 import requests
 import config
@@ -7,6 +7,7 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = config.SECRET_KEY
 
 apikey = config.API_KEY
 db = SQLAlchemy(app)
@@ -15,37 +16,55 @@ class City(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
 
+def get_weather_data(city):
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={apikey}'
+    r = requests.get(url).json()
+    return r
+
 @app.route('/')
 def index_get():
     cities = City.query.all()
-    print(cities)
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid={}'
+    
     weather_data = []
 
     for city in cities: 
 
-        r = requests.get(url.format(city.name,apikey)).json()
+        r = get_weather_data(city.name)
         # print(r)
 
         weather = {
             'city': city.name,
-            'temperature': r['main']['temp'],
+            'temperature': round(r['main']['temp']),
             'icon': r['weather'][0]['icon'],
             'description': r['weather'][0]['description'],
         }
 
         weather_data.append(weather)
-    print("Weather data {}".format(weather_data))
+    # print("Weather data {}".format(weather_data))
     return render_template('weather.html', weather_data=weather_data)
 
 @app.route('/', methods=['POST'])
 def index_post():
+    err_msg = ''
     new_city = request.form.get('city')
 
     if new_city:
-        new_city_obj = City(name=new_city)
-        db.session.add(new_city_obj)
-        db.session.commit()
+        existing_city = City.query.filter_by(name=new_city).first()
+        if not existing_city:
+            new_city_data = get_weather_data(new_city)
 
-    return render_template('weather.html', weather_data=weather_data)
+            if new_city_data['cod']==200:
+                new_city_obj = City(name=new_city)
+                db.session.add(new_city_obj)
+                db.session.commit()
+            else:
+                err_msg = 'City does not exist!'
+        else:
+            err_msg = 'City already exists!'
+   
+    if err_msg:
+        flash(err_msg,'error')
+    else:
+        flash('City added successfully!')
+    return redirect(url_for('index_get'))
 
--- config_example.py
+++ config_example.py
@@ -1,2 +1,3 @@
 # Change filename to config.py and add API key below
 API_KEY = 'YOUR_API_KEY'
+SECRET_KEY = 'YOUR_SECRET_KEY'
